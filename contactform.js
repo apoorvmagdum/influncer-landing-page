aac19fef33a277b3e1768d66c8b52a676feeb4cf
apoorvmagdum <apoorvmagdum2004@gmail.com>
2025-04-22 21:03:31 +0530

    changes

--- contactform.js
+++ contactform.js
@@ -4,6 +4,7 @@ function sendMail(){
     email : document.getElementById("email").value,
     subject : document.getElementById("subject").value,
     message : document.getElementById("message").value,
+    contact : document.getElementById("contact").value,
   }
 
   emailjs.send("service_cnvtjh6","template_qmrz23l",parms).then(alert("Email sent!!"))
